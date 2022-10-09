5b127d36911e02bd00369d48a4eaf15677df3bce
rover0811 <rover0811@hotmail.com>
2022-10-09 22:16:37 +0900

    feat: 하단바 완성(+ navigate 기능)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TabNavigation from "./navigations/TabNavigation";
 import { NavigationContainer } from "@react-navigation/native";
-
+import Hello from "./navigations/Practice";
 export default function App() {
   return (
     <NavigationContainer>
